57e8956e4dd39d357b5dccdcb9987252d0e535ca
Ilya Nidzelsky <inidzelsky@gmail.com>
2020-07-14 11:38:36 +0300

    implement password hashing

--- routes/register.js
+++ routes/register.js
@@ -1,15 +1,21 @@
 'use strict';
 
+const path = require('path');
 const Router = require('@koa/router');
 const multer = require('@koa/multer');
 const thumb = require('node-thumbnail').thumb;
 const jwt = require('jsonwebtoken');
-const path = require('path');
+const bcrypt = require('bcryptjs');
 
+// Get config values
+const config = require(path.join(__dirname, '..', 'config'));
+const { jwtSecret, multerStorage, avatarsPath, thumbnailsPath } = config;
 
-const config = require('../config');
-const { jwtSecret, multerStorage } = config;
+// Get database functions
+const database = require(path.join(__dirname, '..', 'database', 'database'));
+const { saveUser } = database;
 
+// Set "multer" options
 const storage = multer.diskStorage(multerStorage);
 const upload = multer({
   storage,
@@ -31,6 +37,29 @@ const router = new Router();
 router.post('/register', upload.single('avatar'), async ctx => {
   try {
     const { email, password } = ctx.request.body;
+    const { filename } = ctx.request.file;
+
+    // Create avatar and thumbnail file pathes
+    const ext = path.extname(filename);
+    const base = path.basename(filename, ext);
+    const thumbnailFileName = `${base}_thumb${ext}`;
+    const avatarFilePath = path.join(avatarsPath, filename);
+    const thumbnailFilePath = path.join(thumbnailsPath, thumbnailFileName);
+
+    // Create a thumbnail from the avatar
+    await thumb({
+      source: avatarFilePath,
+      destination: thumbnailsPath,
+      width: 100,
+      quiet: true
+    });
+
+    // Generate a password hash
+    const salt = await bcrypt.genSalt(10);
+    const hashPassword = await bcrypt.hash(password, salt);
+
+    // Save user data to db
+    await saveUser(email, hashPassword, avatarFilePath, thumbnailFilePath);
 
     const payload = {
       user: {
@@ -39,6 +68,7 @@ router.post('/register', upload.single('avatar'), async ctx => {
       }
     };
 
+    //Creating token
     const token = jwt.sign(
       payload,
       jwtSecret,
@@ -46,19 +76,13 @@ router.post('/register', upload.single('avatar'), async ctx => {
         expiresIn: 360000
       });
 
-    const avatarUrl = ctx.request.host + '/public/avatars';
+    const avatarUrl = `${ctx.request.host}/${avatarsPath}`;
 
     ctx.body = {
       token,
       avatarUrl
     };
 
-    thumb({
-      source: `./public/avatars/${ctx.file.filename}`,
-      destination: './public/avatars/thumbnails',
-      width: 100,
-      quiet: true
-    });
   } catch(e) {
     console.log(e.message);
   }
