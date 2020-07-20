d19556c5c37e70bf166b87860f6e932842868413
Ilya Nidzelsky <inidzelsky@gmail.com>
2020-07-20 10:32:29 +0300

    get user data with Github API

--- controllers/sendController.js
+++ controllers/sendController.js
@@ -1,7 +1,30 @@
 'use strict';
 
+const path = require('path');
+const axios = require('axios');
+
+const { oAuthToken } = require(path.join(__dirname, '..', 'config'));
+
 const sendEmails = async ctx => {
-  ctx.body = 'Send route';
+  const { userNames } = ctx.request.body;
+
+  const results = await Promise.all(users.map(getUserInfo));
+  const users = results.map(res => ({
+    email: res.data.email,
+    location: res.data.location
+  }));
+
+  ctx.body = users;
+};
+
+const getUserInfo = (username) => {
+  const config = {
+    headers: {
+      Authorization: `token ${oAuthToken}`
+    }
+  };
+
+  return axios.get(`https://api.github.com/users/${username}`, config);
 };
 
 module.exports = {
